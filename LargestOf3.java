3b430995a280ea4473c1473d45f3b1b956832551
Gokul Govinda <87459046+gokulgovinda@users.noreply.github.com>
2023-03-16 23:25:35 +0530

    Create LargestOf3.java

---
+++ LargestOf3.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class LargestOf3 {
+    public static void main(String[] args) {
+     
+      int a=1;
+      int b=3;
+      int c=6;
+        if((a>=b)&&(a>=c)) {
+        	System.out.println("a is largest");
+            
+        }
+        else if (b>=c){
+        	System.out.println("b is largest");
+           
+        }
+        else {
+        	System.out.println("c is largest");
+        }
+        	
+    }
+}
